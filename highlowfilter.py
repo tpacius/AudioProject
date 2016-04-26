8c10723d67c5317c60e0fb25df60004ce3545671
tpacius <tpacius@bu.edu>
2016-04-26 12:19:11 -0400

    Functioned high and low, added bandpass and bandreject, and added more sample wav files to test

--- highlowfilter.py
+++ highlowfilter.py
@@ -6,44 +6,96 @@ fS = 44100  # Sampling rate.
 fL = 4410  # Cutoff frequency.
 N = 59  # Filter length, must be odd.
 
-# Compute sinc filter.
-h = np.sinc(2 * fL / fS * (np.arange(N) - (N - 1) / 2.))
-l = np.sinc(2 * fL / fS * (np.arange(N) - (N - 1) / 2.))
+#Takes in a wav array and a window type
+def lowpassFilter(X, windowType):
+	sincFilter = np.sinc(2 * fL / fS * (np.arange(N) - (N - 1) / 2.))
 
-# Apply window.
-h *= np.blackman(N)
-l *= np.hanning(N)
+	if windowType == "Blackman":
+		sincFilter *= np.blackman(N)
+	if windowType == "Hanning":
+		sincFilter *= np.hanning(N)
 
-# Normalize to get unity gain.
-h /= np.sum(h)
-l /= np.sum(l)
+	sincFilter /= np.sum(sincFilter)
 
-# print(h)
+	return np.convolve(X,sincFilter)
 
-# Applying the filter to a signal s can be as simple as writing
-# s = np.convolve(s, h)
+#Takes in a wav array and a window type
+def highpassFilter(X,windowType):
+	sincFilter = np.sinc(2 * fL / fS * (np.arange(N) - (N - 1) / 2.))
 
-def lowpassFilter(X, h):
-	return np.convolve(X,h)
+	if windowType == "Blackman":
+		sincFilter *= np.blackman(N)
+	if windowType == "Hanning":
+		sincFilter *= np.hanning(N)
 
-def highpassFilter(X,h):
-	h = - h
-	h[(N - 1) / 2] += 1
-	return np.convolve(X,h)
+	sincFilter /= np.sum(sincFilter)
 
-test = au.readWaveFile("Beethoven.Ninth.wav")
+	#spectral inversion from low pass filter
+	sincFilter = -sincFilter
+	sincFilter[(N-1) / 2] += 1
+
+	return np.convolve(X,sincFilter)
+
+#Takes in a wav array and a window type
+def bandPassFilter(X, windowType):
+	low = np.sinc(2 * fL / fS * (np.arange(N) - (N - 1) / 2.))
+
+	if windowType == "Blackman":
+		low *= np.blackman(N)
+	if windowType == "Hanning":
+		low *= np.hanning(N)
+
+	low /= np.sum(low)
+	high = -low
+	high[(N-1)/2] += 1
+
+	bandPass = np.convolve(low, high)
+	return np.convolve(X, bandPass)
+
+#Takes in a wav array and a window type
+def bandRejectFilter(X, windowType):
+	low = np.sinc(2 * fL / fS * (np.arange(N) - (N - 1) / 2.))
+
+	if windowType == "Blackman":
+		low *= np.blackman(N)
+	if windowType == "Hanning":
+		low *= np.hanning(N)
+
+	low /= np.sum(low)
+	high = -low
+	high[(N-1)/2] += 1
+
+	bandReject =  low + high
+	return np.convolve(X, bandReject)
+
+
+#Unit Testing
+test1 = au.readWaveFile("Beethoven.Ninth.wav")
+test2 = au.readWaveFile("BluesGuitar.wav")
+test3 = au.readWaveFile("Clarinet01_01.wav")
+test4 = au.readWaveFile("BassGuitar.wav")
+test5 = au.readWaveFile("Beethoven.Sixth.wav")
 
 #Blackman window
-ret1 = lowpassFilter(test,h)
+ret1 = lowpassFilter(test,"Blackman")
 au.writeWaveFile("LowPassTest.wav",ret1)
-ret2 = highpassFilter(test,h)
+ret2 = highpassFilter(test, "Blackman")
 au.writeWaveFile("HighPassTest.wav",ret2)
+ret3 = bandPassFilter(test, "Blackman")
+au.writeWaveFile("BandPassFilterTest.wav",ret3)
+ret4 = bandRejectFilter(test, "Blackman")
+au.writeWaveFile("BandRejectFilterTest.wav",ret4)
 
 #Hanning window
-ret3 = lowpassFilter(test,l)
-au.writeWaveFile("LowPassTestHanning.wav",ret3)
-ret4 = highpassFilter(test,l)
-au.writeWaveFile("HighPassTestHanning.wav",ret4)
+ret1 = lowpassFilter(test,"Hanning")
+au.writeWaveFile("LowPassTestHanning.wav",ret1)
+ret2 = highpassFilter(test, "Hanning")
+au.writeWaveFile("HighPassTestHanning.wav",ret2)
+ret3 = bandPassFilter(test, "Hanning")
+au.writeWaveFile("BandPassFilterTestHanning.wav",ret3)
+ret4 = bandRejectFilter(test, "Hanning")
+au.writeWaveFile("BandRejectFilterTestHanning.wav",ret4)
+
 
 
 
